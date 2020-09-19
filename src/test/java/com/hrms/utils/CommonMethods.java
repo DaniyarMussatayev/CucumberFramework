0176d4d86ae34c914beac9f995f43d446c01f5e9
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-09-19 23:17:32 +0600

    Developing common functions

--- src/test/java/com/hrms/utils/CommonMethods.java
+++ src/test/java/com/hrms/utils/CommonMethods.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.UnexpectedTagNameException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.hrms.testbase.PageInitializer;
@@ -72,7 +77,7 @@ public class CommonMethods extends PageInitializer {
 	public static void waitForClickability(WebElement element) {
 		getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
 	}
-	
+
 	public static void waitForVisability(WebElement element) {
 		getWaitObject().until(ExpectedConditions.visibilityOf(element));
 	}
@@ -83,33 +88,152 @@ public class CommonMethods extends PageInitializer {
 	}
 
 	/**
-	 * Method that will take a screenshot and store with name in specified location with .png extension
+	 * Method that will take a screenshot and store with name in specified location
+	 * with .png extension
+	 * 
 	 * @param fileName
 	 */
 	public static byte[] takeScreenshot(String fileName) {
 
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		byte[] bytes = ts.getScreenshotAs(OutputType.BYTES);
-		
+
 		File src = ts.getScreenshotAs(OutputType.FILE);
 		try {
-			FileUtils.copyFile(src, new File(Constants.SCREENSHOT_FILEPATH + fileName +getTimeStamp()+ ".png"));
+			FileUtils.copyFile(src, new File(Constants.SCREENSHOT_FILEPATH + fileName + getTimeStamp() + ".png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return bytes;
 	}
-	
+
 	/**
 	 * This method will generate timeStamp
+	 * 
 	 * @return
 	 */
 	public static String getTimeStamp() {
-		
-		Date date=new Date();
-		
-		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
-		
+
+		Date date = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
+
 		return sdf.format(date);
 	}
+
+	/**
+	 * this method will click on check or radio button by the given list of
+	 * webelement and the value
+	 * 
+	 * @param radioOrCheckBoxes
+	 * @param value
+	 */
+	public static void clickRadioOrCheckBox(List<WebElement> radioOrCheckBoxes, String value) {
+		String actualValue;
+		for (WebElement radioOrCheckBox : radioOrCheckBoxes) {
+			actualValue = radioOrCheckBox.getAttribute("value").trim();
+			if (radioOrCheckBox.isEnabled() && actualValue.equals(value)) {
+				jsClick(radioOrCheckBox);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * this method will select an option from the dropdown by the given webelement
+	 * and visible text value
+	 * 
+	 * @param dd
+	 * @param visibleTextOrValue
+	 */
+	public static void selectDDValue(WebElement dd, String visibleTextOrValue, int index) {
+		try {
+			Select select = new Select(dd);
+			List<WebElement> options = select.getOptions();
+			for (WebElement option : options) {
+				if (option.getText().equals(visibleTextOrValue)) {
+					select.selectByVisibleText(visibleTextOrValue);
+					break;
+				}
+			}
+
+		} catch (UnexpectedTagNameException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * this method will select an option from the dropdown by the given webelement
+	 * and visible text value
+	 * 
+	 * @param dd
+	 * @param index
+	 */
+	public static void selectDDValue(WebElement dd, int index) {
+		try {
+			Select select = new Select(dd);
+			List<WebElement> options = select.getOptions();
+			int size = options.size();
+			if (size > index) {
+				select.selectByIndex(index);
+			}
+			
+		} catch (UnexpectedTagNameException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	/**
+	 * this method will switch to a frame by the given frame webelement
+	 * @param iFrame
+	 */
+	public static void switchToFrame(WebElement iFrame) {
+		try {
+			driver.switchTo().frame(iFrame);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * this method will switch to a frame by the given frame index
+	 * @param frameIndex
+	 */
+	public static void switchToFrame(int frameIndex) {
+		try {
+			driver.switchTo().frame(frameIndex);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * this method will switch to a frame by the given frame nameOrId
+	 * @param nameOrId
+	 */
+	public static void switchToFrame(String nameOrId) {
+		try {
+			driver.switchTo().frame(nameOrId);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * this method will switch to Child Window
+	 */
+	public static void switchToChildWindow() {
+		String mainWindow =driver.getWindowHandle();
+		Set<String> allWindows =driver.getWindowHandles();
+		for (String window : allWindows) {
+			if(!window.equals(mainWindow)) {
+				driver.switchTo().window(window);
+				break;
+			}
+		}
+	}
+
 }
